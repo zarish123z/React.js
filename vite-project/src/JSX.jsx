6eb5df8ec098c6ab8d2d8962d1d4918367ac15d4
zarish123z <zarish.zafar100@gmail.com>
2025-10-09 16:49:24 +0500

    BC

---
+++ vite-project/src/JSX.jsx
@@ -0,0 +1,26 @@
+function JSX() {
+  const name = "Welcome to Vite + React ";
+  const clicked = () => alert(`Hello, ${name}!`);
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "px" }}>
+      
+      <h1 style={{ color: "red" }}> {name}!</h1>
+      <button
+        onClick={clicked}
+        style={{
+            backgroundColor :"royalblue",
+          padding: "20px 30px",
+          fontSize: "20px",
+          borderRadius: "15px",
+          cursor:"col-resize",
+
+        }}
+      >
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default JSX;
