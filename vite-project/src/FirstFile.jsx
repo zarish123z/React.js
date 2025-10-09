68eccf7a022efb9e70a79cdbcad1cf1f7ee4c604
zarish123z <zarish.zafar100@gmail.com>
2025-10-09 12:13:27 +0500

    functioncomponeent

---
+++ vite-project/src/FirstFile.jsx
@@ -0,0 +1,14 @@
+
+
+function FirstFile(props){
+    return(
+        <div>
+        <h1>Hello, {props.name}!</h1>
+      <p>This is my first functional component.</p>
+            <h3>Age: {props.age}</h3>
+
+        </div>
+       
+    )
+}
+export default FirstFile
