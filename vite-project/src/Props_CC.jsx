161cf15b0caee85969963fd12ce09115936b8da0
zarish123z <zarish.zafar100@gmail.com>
2025-10-11 14:35:26 +0500

    Props Class_Component

--- vite-project/src/Props_CC.jsx
+++ vite-project/src/Props_CC.jsx
@@ -4,12 +4,12 @@ import React, { Component } from "react";
 class Props_CC extends Component {
   render() {
     //  props using this.props
-    const { name, course } = this.props;
+    // const{name,course}= this.props
 
     return (
       <div>
-        <h2>Welcome, {name}!</h2>
-        <p>You are learning: {course}</p>
+        <h2>Welcome, {this.props.name}!</h2>
+        <p>You are learning: {this.props.course}</p>
       </div>
     );
   }
