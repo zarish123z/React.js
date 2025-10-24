33ce396aeb5779b315dd4fc7252d05ffdd014a35
zarish123z <zarish.zafar100@gmail.com>
2025-10-24 16:23:31 +0500

    Deleted AllUseEffectExamples.jsx

--- vite-project/src/UseEffect.jsx
+++ vite-project/src/UseEffect.jsx
@@ -9,29 +9,10 @@ function UseEffect() {
         document.title=`You clicked${count} times`; // Update browser title
     },[count])  // run when count changes
   return (
-    <div style={{
-        textAlign: "center",
-        marginTop: "100px",
-        background: "#f9f9f9",
-        padding: "30px",
-        borderRadius: "10px",
-        boxShadow: "0 0 10px gray",
-      }}>
-      <h2> UseEffect</h2>
-      <p >You Clicked {count} times</p>
-    <button onClick={() => setcount(count + 1)}
-       style={{
-          background: "#007bff",
-          color: "white",
-          border: "none",
-          padding: "10px 20px",
-          fontSize: "16px",
-          borderRadius: "5px",
-          cursor: "pointer",
-          
-        }} 
-        >Click_Me
-        </button>
+    <div className="container">
+      <h2 className="heading"> UseEffect</h2>
+      <p className="text">You Clicked {count} times</p>
+    <button onClick={() => setcount(count + 1)}>Click_Me</button>
 
     </div>
   )
