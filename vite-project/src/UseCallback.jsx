d9b899c57d3b2784f8b64fc5c59fdd6cc755cc43
zarish123z <zarish.zafar100@gmail.com>
2025-11-03 22:48:33 +0500

    UseCallback_(Hook)

---
+++ vite-project/src/UseCallback.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useCallback } from "react";
+
+//  product list
+const products = ["Apple", "Banana", "Mango", "Grapes", "Orange", "Papaya"];
+
+function UseCallback() {
+  const [search, setSearch] = useState("");
+
+  //  useCallback -> filtering function memoized
+  // Only re-creates when `search` value changes
+  const filteredProducts = useCallback(() => {
+    return products.filter((item) =>
+      item.toLowerCase().includes(search.toLowerCase())
+    );
+  }, [search]);
+
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "80px",
+        padding: "30px",
+        width: "350px",
+        marginInline: "auto",
+        background: "#e8f5e9",
+        borderRadius: "10px",
+        boxShadow: "0 0 10px gray",
+      }}
+    >
+      <h2> useCallback Search Filter</h2>
+
+      {/* Input changes -> only search state update */}
+      <input
+        type="text"
+        placeholder="Search fruit..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        style={{
+          padding: "10px",
+          width: "80%",
+          borderRadius: "5px",
+          border: "1px solid gray",
+          marginBottom: "15px",
+        }}
+      />
+
+      {/*  Call memoized function */}
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        {filteredProducts().map((item, index) => (
+          <li
+            key={index}
+            style={{
+              padding: "6px",
+              background: "#ffffffff",
+              margin: "4px 0",
+              borderRadius: "5px",
+              boxShadow: "0 0 5px lightgray",
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UseCallback;
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -20,6 +20,7 @@ import UseContext from "./UseContext.jsx";
 import AppInsideuseContext from "./AppInsideuseContext.jsx";
 import UseReducer from "./UseReducer.jsx";
 import UseMemo from "./UseMemo.jsx";
+import UseCallback from "./UseCallback.jsx";
 
 
 
@@ -49,8 +50,9 @@ createRoot(document.getElementById("root")).render(
     {/* <UseContext>
   <AppInsideuseContext/>
 </UseContext> */}
-<UseReducer/>
+{/* <UseReducer/> */}
 {/* <UseMemo/> */}
+<UseCallback/>
 
 
 
